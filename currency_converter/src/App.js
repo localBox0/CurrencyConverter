dc64edee9369f8eaaca354c57bf8fb114aa3b7ec
LocalBand <agvrideteam@gmail.com>
2023-03-05 17:05:59 +0200

    Initialized the application using React Create App.
    - The main page with state control has been created.
    - Added a child block to display the exchange table interface.
    
    - Added application styling.

---
+++ currency_converter/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { Block } from './components/Block'
+import './index.css'
+import {useEffect, useState} from "react";
+
+
+function App() {
+    const [fromCurrency, setFromCurrency] = useState('UAH')
+    const [toCurrency, setToCurrency] = useState('USD')
+
+    const [rates, setRates] = useState({})
+
+    useEffect(() => {
+        fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+            .then((res) => res.json())
+            .then((json) => {
+                setRates(json)
+                console.log(json[0].cc)
+            })
+            .catch((err) => {
+                console.warn(err)
+                alert('Не вдалося отримати інформацію')
+            })
+
+    }, [])
+
+    return (
+        <div className="App">
+            <Block value={0} currency={fromCurrency} onChangeCurrency={setFromCurrency}/>
+            <Block value={0} />
+        </div>
+    );
+}
+
+export default App;
